bdaf940c9342e0be051a352f28c0134c1aef0e08
Victor <vlopezmoles@gmail.com>
2022-05-10 13:01:40 +0200

    Actualitzo la entrega 1.2

--- entrega1.2.js
+++ entrega1.2.js
@@ -4,3 +4,39 @@
 
 let sumaAuto = ((a, b) => console.log(a + b))(10, 2);
 
+//Nivell 2
+
+//Exercici 1
+
+let paramNom = (name => ({nom: name}));
+
+console.log(paramNom("Victor"));
+
+//Exercici 2
+
+class Persona {
+    constructor(nom) {
+        this.nom = nom;
+    }
+
+    dirNom() {
+        console.log(this.nom);
+    }
+}
+
+const funcioNom = new Persona("Victor");
+funcioNom.dirNom();
+
+
+//Nivell 3
+
+//Exercici 1
+
+function pelicules(titol) {
+    this.titol = titol;
+}
+
+let cine = new pelicules("El padrino");
+
+console.log(cine);
+
