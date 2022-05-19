e64f1c2912da7b966c9e804156a20fdb3028a355
Victor <vlopezmoles@gmail.com>
2022-05-19 17:22:26 +0200

    Actualització exercicis entrega 1.5

--- entrega1.5/entrega1.5.js
+++ entrega1.5/entrega1.5.js
@@ -48,6 +48,7 @@ const recursivitat = (num) => {
     let missatge = `Queden ${num} segons per finalitzar`;
     setTimeout(() => {
         if(num === 0){
+            console.log('Finalitzat amb exit!')
             return;
         }
         if(num > 0){
@@ -55,6 +56,7 @@ const recursivitat = (num) => {
         return recursivitat(num -1);
         }
     },1000);
+    
 }
 
 recursivitat(5);  
@@ -62,3 +64,60 @@ recursivitat(5);
 //Exercici 2
 
 
+const { exec } = require('child_process');
+
+exec('dir', (error, stdout, stderr) => {
+    if(error) {
+        console.log(`Error: ${error}`)
+        return
+    }
+    if(stderr) {
+        console.log(`stderr: ${error}`);
+        return
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+//Nivell 3
+
+//Exercici 1
+
+//Codifiquem
+
+const codificar = () => {
+    fs.readFile('exercici_1.5.txt', 'hex', (error, data) => {
+        if(!error) {
+            fs.writeFile('exercici_hex.txt', data, (error) => {
+                if(error) {
+                    console.log(`Error: ${error}`);
+                }else {
+                    console.log(`S'ha creat un arxiu en hexadecimal`);
+                }
+            });
+        } else {
+            console.log(`Error: ${error}`);
+        }
+    });
+    fs.readFile('exercici_1.5.txt', 'base64', (error, data) => {
+        if(!error) {
+            fs.writeFile('exercici_base64.txt', data, (error) => {
+                if(error) {
+                    console.log(`Error: ${error}`);
+                }else {
+                    console.log(`S'ha creat un arxiu en base64`);
+                }
+            });
+        } else {
+            console.log(`Error: ${error}`);
+        }
+    });
+}
+    
+codificar();
+    
+
+//Encriptem
+
+const crypto = require('crypto');
+const tipus = 'aes-192-cbc';
+    
