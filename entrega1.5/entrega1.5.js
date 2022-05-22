3ea983ab3d3f8738a46a04f2aec841aec91ba576
Victor <vlopezmoles@gmail.com>
2022-05-22 23:48:55 +0200

    Actualitzo la entraga 1.5

--- entrega1.5/entrega1.5.js
+++ entrega1.5/entrega1.5.js
@@ -1,7 +1,7 @@
 // Nivell 1
 
 // Exercici 1
-
+/* 
 let fs = require('fs');
 
 let text = `Estic escrivint en un arxiu de textos des de Visual Studio Code amb Nodejs`;
@@ -113,11 +113,67 @@ const codificar = () => {
     });
 }
     
-codificar();
+codificar(); 
     
-
+ */
 //Encriptem
 
-const crypto = require('crypto');
-const tipus = 'aes-192-cbc';
-    
+//Comprobem si tenim el modul cryto activat.
+
+let crypto;
+try {
+    crypto = require('crypto');
+} catch (err) {
+    console.log('crypto support is disabled!');
+}
+
+//encryptem els arxius
+
+const {
+    createReadStream,
+    createWriteStream,
+    fstat,
+} = require('fs');
+
+const {
+    pipeline
+} = require('stream');
+
+const {
+    scrypt,
+    randomFill,
+    createCipheriv,
+} = require('crypto');
+
+const algorith = 'aes-192-cbc';
+const password = 'Hello World';
+
+scrypt(password, 'salt', 24, (error, key) => {
+    if(error) console.log(error);
+    randomFill(new Uint8Array(16), (error, iv) => {
+        if(error) console.log(error);
+
+        const cipher = createCipheriv(algorith, key, iv);
+
+        const input = createReadStream('exercici_base64.txt');
+
+        const output = createWriteStream('exercici_base64.enc');
+
+        const input2 = createReadStream('exercici_hex.txt');
+
+        const output2 = createWriteStream('exercici_hex.enc');
+
+        pipeline(input, cipher, output, (error) => {
+            error ? console.log(`Error: ${error}`) : console.log(`El fitxer ha estat encriptat amb exit`);
+        });
+        pipeline(input2, cipher, output2, (error) => {
+            error ? console.log(`Error: ${error}`) : console.log(`El fitxer ha estat encriptat amb exit`);
+        })
+    });
+});
+
+const eliminarFitxers = () => {
+    fs.unlink('exercici_base64.txt', (error) => {
+        
+    });
+}
