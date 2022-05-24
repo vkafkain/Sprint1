f24e9fdceb6fb0b3ee4d93798946b079ad16bc14
Victor <vlopezmoles@gmail.com>
2022-05-24 21:14:13 +0200

    Actualitzo la entrega1.6

---
+++ entrega1.6/app/operacions.js
@@ -0,0 +1,20 @@
+const sumar = (a, b) => {
+    return a + b
+}
+const restar = (a, b) => {
+    return a - b
+}
+const multiplicar = (a, b) => {
+    return a * b
+}
+const dividir = (a, b) => {
+    return a / b
+}
+
+module.exports = {
+    sumar,
+    restar,
+    dividir,
+    multiplicar
+}
+
