82beac7f152ad61246aee3a163fef11e294d7f24
Victor <vlopezmoles@gmail.com>
2022-05-26 23:19:34 +0200

    Actualitzo entrega1.6

---
+++ entrega1.6/__mocks__/AsyncJson.js
@@ -0,0 +1,43 @@
+
+//Modifiquem el exercici per poder rebre l'arxiu JSON.
+
+const {employees} = require('../employeesSalaries.json');
+const {salaries} = require('../employeesSalaries.json');
+
+const getEmployeeJSON = (id) => {
+    return new Promise((resolve, reject) => {
+        let buscat = employees.findIndex(employees => employees.id == id);
+            if(buscat >= 0) {
+                let empleat = employees[buscat];
+                resolve(empleat);
+            } else {
+                reject(`El id ${id} no es troba a la base de dades`);
+            }
+    });
+}
+const getSalaryJSON = (employee) => {
+    return new Promise((resolve, reject) => {
+        let buscat = salaries.findIndex(salaries => salaries.id == employee.id);
+                if(buscat >=0) {
+                    resolve(salaries[buscat].salary);
+                } else {
+                    reject('Empleat no trobat');
+        }
+    });
+}
+
+const funcioAsincrona = async (id) => {
+    try {
+        const treballador = await getEmployeeJSON(id);
+        const salari = await getSalaryJSON(treballador);
+        return console.log(`treballador: ${treballador.name}, sou: ${salari}`);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+module.exports = {
+    getEmployeeJSON,
+    getSalaryJSON,
+    funcioAsincrona,
+}
---
+++ entrega1.6/__test__/asyncJson.test.js
@@ -0,0 +1,4 @@
+/*Refès l'exercici Async / Await Nivell 1 accedint a un fitxer extern JSON.
+Crea tests que demostrin la correcta execució de l'exercici fent un mock del fitxer JSON.*/
+
+
